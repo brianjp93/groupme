be48d1a08c04f9f2ed9b6056ad8c73ff56f9714c
Brian Perrett <perrettbrian@gmail.com>
2018-04-23 22:33:11 -0700

    add oauth url request method

--- groupme.py
+++ groupme.py
@@ -11,8 +11,22 @@ class Client:
 	base = 'https://api.groupme.com/v3'
 	headers = {'Content-Type': 'application/json'}
 
-	def __init__(self, token):
+	def __init__(self, token, client_id=None):
 		self.token = token
+		self.client_id = client_id
 
 		# resources
 		self.group = resources.Group(self)
+
+	def get_auth_url(self):
+		"""Get URL for granting access to a user's data.
+
+		Returns
+		-------
+		str
+
+		"""
+		url = 'https://oauth.groupme.com/oauth/authorize?client_id={}'.format(self.client_id)
+		return url
+
+	
